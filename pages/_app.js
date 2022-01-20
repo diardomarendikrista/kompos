4d886f0b85dce7e99b4a8133767b6d20c51c1e74
diardomarendikrista <diardomarendikrista@gmail.com>
2022-01-20 09:30:10 +0700

    add loading top bar

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,13 @@
 import '../styles/globals.css';
 import 'bootstrap/dist/css/bootstrap.css';
+import "nprogress/nprogress.css";
+import "../styles/nprogress.css";
+import Router from "next/router";
+import nProgress from "nprogress";
+
+Router.events.on("routeChangeStart", nProgress.start);
+Router.events.on("routeChangeError", nProgress.done);
+Router.events.on("routeChangeComplete", nProgress.done);
 
 function MyApp({ Component, pageProps }) {
 	return (
